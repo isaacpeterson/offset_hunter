37808081efc76565a8c04ccf8ad0e3b377552b62
isaacpeterson <isaac.p.mail@gmail.com>
2018-08-30 14:27:34 +1000

    update to match repo

--- run_offset_simulation_hunter.R
+++ run_offset_simulation_hunter.R
@@ -8,5 +8,6 @@ user_output_params <- initialise_user_output_params()
 
 osim.run(user_global_params, user_simulation_params, user_feature_params, loglevel = 'TRACE')
 
-current_simulation_folder = find_current_run_folder(base_folder = '~/offset_data/hunter/')
-osim.output(user_output_params, current_simulation_folder, loglevel = 'TRACE')
+simulation_folder = find_current_run_folder(base_folder = '~/offset_data/hunter/', file_placeholder_width = 3)
+
+osim.output(user_output_params, simulation_folder, loglevel = 'TRACE')
